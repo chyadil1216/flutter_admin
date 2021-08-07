7d345b8e82032467f5915d1c29538ebe0e3a1c95
chyadil1216 <chyadil1216@gmail.com>
2021-08-07 18:03:20 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutteradmin/screens/admin.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Admin(),
+  ));
+}
